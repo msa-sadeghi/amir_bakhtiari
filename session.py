c68d0634fcdc92f743c4814e78afa446383a55bc
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-12-16 21:01:54 +0330

    s

---
+++ session.py
@@ -0,0 +1,50 @@
+# print("hello world")
+# print(1.5)
+
+# # print('he said "blallala"')
+# print("he said \"blallala\"")
+# print('''he said "blallala"''')
+# print("""he said blalal
+#       sfsdf
+#       """)
+# print('''he said blalal
+#       sfsdf
+#       ''')
+
+# print(1)
+
+# print(1 + 2)
+
+# x1 = 12
+# x2 = 13
+# z =  x1 + x2
+# z =  x1 - x2
+# z =  x1 * x2
+# z =  x1 / x2
+# z =  x1 // x2
+# z =  x1 % x2
+# print(x1, x2, z)
+
+# y1 = 12
+# print(type(y1))
+# y1 = 12.2
+# print(type(y1))
+# x1 = "12.2"
+
+# import random
+
+# x = random.random()
+# x = random.randint(1, 10)
+# x = random.randrange(1, 10)
+# print(x)
+
+# import math
+
+name =  "amir"
+# print(name + " " + "bakhtiari")
+# print(f"{name} bakhtiari")
+delimiter = "-"
+print(delimiter.join("amir"))
+
+print(name * 10)
+print(10 * name)
