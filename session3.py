008a64c4afc0d3012f0666fed16682bfbca5e50c
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-12-30 20:58:20 +0330

    s

---
+++ session3.py
@@ -0,0 +1,56 @@
+# price = float(input("enter the price: "))
+# total = price - 0.1 * price
+# print(total)
+
+# print(4 % 2 == 0)
+# print(5 % 2 == 0)
+
+# if 4 % 2 == 0:
+#     print("even")
+# else:
+#     print("odd")
+
+
+# print("hello", end=" ")
+# print("hi")
+
+# print("hello\n" * 5)
+
+
+# age = int(input("enter your age: "))
+# if age >= 18:
+#     print("ok")
+#     print("blalala")
+# elif age <= 13 and age >= 10 :
+#     print("+++++")
+# else:
+#     print("----------")
+# print(True and True and True)
+# print(True and False)
+# print(False and True)
+# print(False and False)
+# print(True or True or True)
+# print(True or False)
+# print(False or True)
+# print(False or False)
+
+# print(not True)
+# print(not False)
+
+# counter = 3
+# print(not(counter == 3))
+# x = 14
+# if not x >= 10 and not x <= 15:
+#     print("ok")
+
+# username = input("enter the username: ")
+# if not (username[0] == "a"):
+#     print("ok")
+
+# if " ":
+#     print("ok")
+
+
+username = input("enter the username: ")
+if not username:
+    print("username cant be empty")
