63fca5803e725fea465f2fc4f345b6804ffaa83b
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2026-01-06 21:00:55 +0330

    codes

---
+++ session4.py
@@ -0,0 +1,23 @@
+# from decimal import Decimal
+
+# print(Decimal("0.1") + Decimal("0.2") == Decimal("0.3"))
+
+# # for i in range(1,10,2):
+# #     print(i)
+
+# for i in range(10,-1,-1):
+#     print(i)
+
+
+# sum = 0
+# for i in range(2,  100, 2):
+#     # sum = sum + i
+#     sum += i
+# print(sum)
+
+name = input("enter the name: ")
+# for i in range(len(name)):
+#     print(name[i])
+
+for item in enumerate(name):
+    print(item[0], item[1])
